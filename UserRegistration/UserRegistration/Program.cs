859fec1a825cbe4fea87113ba9078b7e654d2b4c
IndiraShahi <IndiraShahi144@gmail.com>
2021-07-10 09:49:08 +0530

    [Indira] ADDED: Test cases for all the entries

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -2,7 +2,7 @@
 
 namespace UserRegistration
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
--- UserRegistration/UserRegistration/RegexTest.cs
+++ UserRegistration/UserRegistration/RegexTest.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 namespace UserRegistration
 {
-    class RegexTest
+    public class RegexTest
     {
         public string firstName = "^[A-Z]{1}[a-zA-z]{2,10}";
         public string lastName = "^[A-Z]{1}[a-zA-z]{2,10}";
